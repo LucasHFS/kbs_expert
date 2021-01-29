963d776ac380846df204f0fdb3d0ee48798f2723
Lucas Silva <lucash.silva1612@gmail.com>
2021-01-29 19:05:12 -0300

    Some Changes

--- index.js
+++ index.js
@@ -3,6 +3,101 @@ var expert = require("./lib/expert.js");
 
 var {doe, sin, verb} = require('./config/knowledgeBase');
 
+var readline = require('readline-sync');
+
+var sintomas = [], possiveis_doencas = [];
+
+
+function getOption(){
+    console.log("\n---------------- SINTOMAS CADASTRADOS ----------------");
+    console.log("\n",sintomas, "\n");
+    console.log("\n---------------- M E N U  D E  A Ç Õ E S ----------------")
+    let option = ['Adicionar um Sintoma', 'Remover um Sintoma', 'Pesquisar Doenças Relacionadas', 'Zerar Doenças'];
+    let index = 1 + readline.keyInSelect(option, 'Selecione uma Ação:');
+    if(index !== 0){
+        switch(index){
+            case 1: addSintomas();
+                break;
+
+            case 2: removerSintomas();
+                break;
+
+            case 3: loadDoencas();
+                break;
+            
+            case 4: resetSintomas();
+                break;
+        }
+        
+        
+    }else{
+        console.log("Finalizando ...");
+        return
+    }
+}
+
+function main(){
+    getOption();
+    while(index !== 0){
+        getOption();
+    }
+    
+   
+}
+
+function addSintomas(){
+    console.log("\n---------------- S I N T O M A ----------------")
+    let sintoma = readline.question("\nDigite um Sintoma: ");
+    sintomas = [...sintomas, sintoma];
+    console.log("\n",sintomas, "\n");
+    console.log("\n", "Possíveis Doenças",possiveis_doencas, "\n");
+}
+
+function removerSintomas(){
+    console.log("\n---------------- S I N T O M A ----------------")
+    let indexSintoma = readline.keyInSelect(sintomas, 'Selecione um Sintoma:');
+
+    if(indexSintoma >= 0){
+        let boolYesOrEmpty = readline.keyInYN();
+
+        if(boolYesOrEmpty)
+            //TODO: remove this fucking Index
+            array.splice(indexSintoma, 1);
+
+        console.log("\n",sintomas, "\n");
+    }
+}
+
+function resetSintomas(){
+    boolYesOrEmpty = readline.keyInYN()
+    if(boolYesOrEmpty)
+        sintomas = [];
+}
+
+function loadDoencas(){
+
+}
+
+main();
+
+
+
+// Search
+
+// const keywordArr = ['jim', 'john'];
+
+// const message = 'Hello john, how are you doing today?'
+
+// let has = keywordArr.reduce((r,v) => message.toLowerCase().includes(v.toLowerCase()) || r, false)
+
+// console.log(has)
+
+
+
+
+
+
+
 console.log("what has fur?");
 
 var answer1 = verb.whatHas(sin.fur);
--- prompt/index.js
+++ prompt/index.js
@@ -1,31 +1,88 @@
-// Search
+var readline = require('readline-sync');
 
-const prompt = require('prompt');
+var sintoma = '', option = 0, index, sintomas = [], possiveis_doencas = [];
 
-prompt.start();
 
-prompt.get(['username', 'email'], function (err, result) {
-    if (err) { return onErr(err); }
-    console.log('Command-line input received:');
-    console.log('  Username: ' + result.username);
-    console.log('  Email: ' + result.email);
-});
+function getOption(){
+    console.log("\n---------------- SINTOMAS CADASTRADOS ----------------");
+    console.log("\n",sintomas, "\n");
+    console.log("\n---------------- M E N U  D E  A Ç Õ E S ----------------")
+    option = ['Adicionar um Sintoma', 'Remover um Sintoma', 'Pesquisar Doenças Relacionadas', 'Zerar Doenças'],
+    index = 1 + readline.keyInSelect(option, 'Selecione uma Ação:');
+    if(index !== 0){
+        switch(index){
+            case 1: addSintomas();
+                break;
 
-function onErr(err) {
-    console.log(err);
-    return 1;
+            case 2: removerSintomas();
+                break;
+
+            case 3: loadDoencas();
+                break;
+            
+            case 4: resetSintomas();
+                break;
+        }
+        
+        
+    }else{
+        console.log("Finalizando ...");
+        return
+    }
 }
 
+function main(){
+    getOption();
+    while(index !== 0){
+        getOption();
+    }
+    
+   
+}
 
+function addSintomas(){
+    console.log("\n---------------- S I N T O M A ----------------")
+    var sintoma = readline.question("\nDigite um Sintoma: ");
+    sintomas = [...sintomas, sintoma];
+    console.log("\n",sintomas, "\n");
+    console.log("\n", "Possíveis Doenças",possiveis_doencas, "\n");
+}
+
+function removerSintomas(){
+    console.log("\n---------------- S I N T O M A ----------------")
+    let indexSintoma = readline.keyInSelect(sintomas, 'Selecione um Sintoma:');
+
+    if(indexSintoma >= 0){
+        let boolYesOrEmpty = readline.keyInYN();
+
+        if(boolYesOrEmpty)
+            //TODO: remove this fucking Index
+            array.splice(index, 1);
+
+        console.log("\n",sintomas, "\n");
+    }
+}
+
+function resetSintomas(){
+    boolYesOrEmpty = readline.keyInYN()
+    if(boolYesOrEmpty)
+        sintomas = [];
+}
+
+function loadDoencas(){
+
+}
+
+main();
 
 
 
 // Search
 
-const keywordArr = ['jim', 'john'];
+// const keywordArr = ['jim', 'john'];
 
-const message = 'Hello john, how are you doing today?'
+// const message = 'Hello john, how are you doing today?'
 
-let has = keywordArr.reduce((r,v) => message.toLowerCase().includes(v.toLowerCase()) || r, false)
+// let has = keywordArr.reduce((r,v) => message.toLowerCase().includes(v.toLowerCase()) || r, false)
 
-console.log(has)
+// console.log(has)
